433c73f76f742fec5a678774d62f714b6d155a5d
Adolfo Hernandez Sanchez <a01782496@tec.mx>
2026-02-25 18:07:43 -0600

    Añadí files de la tarea al repo donde estaré trabajando

---
+++ Videojuegos/Tarea1/JavaScript/js/hello.js
@@ -0,0 +1,21 @@
+/*
+My first JS program
+
+Adolfo Hernández Sánchez
+16/02/26
+*/ 
+
+console.log("Hello world");
+
+let name = "Gil";
+var surname = "Echeverria";
+const school = "Tec";
+
+console.log(`Good boyyyy ${name}`);
+
+function test(a, b){
+    return a + b;
+}
+
+console.log(test(4, 5));
+
---
+++ Videojuegos/Tarea1/JavaScript/js/tc2005b.js
@@ -0,0 +1,41 @@
+/*
+ * Actividad en clase: Javascript
+ * Adolfo Hernández Sánchez
+ * A0182496
+ */
+
+"use strict";
+
+function firstNonRepeating(str){
+    /*
+    const count = {};
+
+    for (const char of str) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    for (const char of str) {
+        if (count[char] === 1) return char;
+    }*/
+
+}
+
+
+export {
+    firstNonRepeating,
+    /*bubbleSort,
+    invertArray,
+    invertArrayInplace,
+    capitalize,
+    mcd,
+    hackerSpeak,
+    factorize,
+    deduplicate,
+    findShortestString,
+    isPalindrome,
+    sortStrings,
+    stats,
+    popularString,
+    isPowerOf2,
+    sortDescending,*/
+};
