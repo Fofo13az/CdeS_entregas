01dd446f211063505911fa2d55782bf6fe9584e8
Adolfo Hernandez Sanchez <a01782496@tec.mx>
2026-03-25 23:15:55 -0600

    Fofo Breakout juego terminado

---
+++ Videojuegos/Breakout/README.md
@@ -0,0 +1,41 @@
+# Fofo Breakout
+
+### The classic arcade game... with a twist!
+
+The classic arcade game Breakout where the player controls a paddle at the bottom of the screen and bounces a ball on it to destroy all the blocks at the top of the screen now comes with a couple of extra features which will make the original experience more fun... and challenging!
+
+Created by Fofo13az (Adolfo Hernández)
+
+## How to Start
+Since this game was created using JS and HTML, you have to run the **breakout.html** file found in this same repository to start playing.
+
+## How to Play
+
+- Press **Space** to serve the ball
+- Use **A** and **D** to move the paddle left and right
+- Bounce the ball off your paddle to hit and destroy blocks
+- If the ball falls below the screen, you lose a life
+- Press **R** to restart after a game over
+
+## Rules
+
+- You start with **3 lives**
+- Each block destroyed awards **1 point**
+- Losing all lives ends the game in a **loss**
+- Destroying all blocks ends the game in a **win**
+
+## Objective
+
+Destroy ALL the blocks before running out of lives.
+
+## ...the TWIST!
+
+Fofo Breakout introduces TWO new mechanics to the game, making it more challenging:
+
+1. **Paddle shrinking**: Every time you destroy an entire row of blocks (or the equivalent 8 blocks) your paddle will shrink slightly, until it reaches a minimum size of 50px. This will make it harder for you to keep the ball bouncing!
+   
+2. **Speed increase**: At the same intervals, both the ball speed and paddle speed gradually increase, giving you less reaction time as the game progresses!
+
+These mechanics ensure the game gets harder the closer you are to winning, creating a fun, challenging and refreshing version of Breakout.
+
+## Enjoy!
--- Videojuegos/Breakout/breakout.js
+++ Videojuegos/Breakout/breakout.js
@@ -6,12 +6,12 @@ const canvasHeight = 800;
 let ctx;
 let game;
 
-let oldTime = 0;
+let oldTime = 0; 
 let playerSpeed = 0.5;
-let ballSpeed = 0.5;
+let ballSpeed = 0.4;
 
 // Block grid configuration
-const blockRows = 6;
+const blockRows = 7;
 const blockCols = 8;
 const blockWidth = 88;
 const blockHeight = 25;
@@ -128,18 +128,29 @@ class Game {
         this.points = 0;
         this.livesStr = "Lives: ";
         this.lives = 3;
+        this.youStr = "You";
+        this.loseStr = "Lose!";
+        this.winStr = "Win!";
+        this.restartStr = "Press 'r' to restart"
 
+        this.playerSize = 200;
+
+        this.gameOver = false;
+        this.gameRestart = false;
+
+        this.audio = document.createElement("audio");
+        this.audio.src = "audio/4387__noisecollector__pongblipe4.wav";
 
         this.createEventListeners();
-        this.initObjects();
+        this.initObjects(this.playerSize);
 
 
     }
 
-    initObjects() {
+    initObjects(playerSize) {
         this.background = new GameObject(new Vector(canvasWidth / 2, canvasHeight / 2), canvasWidth, canvasHeight, "black");
 
-        this.player = new Player(new Vector(canvasWidth/2, canvasHeight - 50), 200, 20, "yellow");
+        this.player = new Player(new Vector(canvasWidth/2, canvasHeight - 50), playerSize, 20, "yellow");
 
         this.ball = new Ball(new Vector(canvasWidth/2, canvasHeight/2), 20, 20, "white");
 
@@ -159,28 +170,49 @@ class Game {
 
         this.livesStrText = new TextLabel(canvasWidth - canvasWidth/8 - 80, 35, "35px Courier New", "white");
         this.livesText = new TextLabel(canvasWidth - canvasWidth/8 + 50, 35, "35px Courier New", "white");
-    
+
+        this.youText = new TextLabel(canvasWidth/2 - 115, canvasHeight/2 - 50, "120px Courier New", "white");
+        this.loseText = new TextLabel(canvasWidth/2 - 165, canvasHeight/2 + 50, "120px Courier New", "white");
+        this.winText = new TextLabel(canvasWidth/2 - 130, canvasHeight/2 + 50, "120px Courier New", "white");
+        this.restartText = new TextLabel(canvasWidth/2 - 180, canvasHeight - 50, "30px Courier New", "white");
+
+    }
+
+    initPlayer(size){
+        
     }
 
     draw(ctx) {
         // Draw the background first, so everything else is drawn on top
         this.background.draw(ctx);
-    
-        this.player.draw(ctx);
 
-        for (let block of this.blocks) {
-            if (block.isBroken == false){
-                block.draw(ctx);
+        if (!this.gameOver){
+            this.player.draw(ctx);
+
+            for (let block of this.blocks) {
+                if (block.isBroken == false){
+                    block.draw(ctx);
+                }
             }
+
+            this.ball.draw(ctx);
+
+            this.pointsStrText.draw(ctx, this.pointsStr);
+            this.pointsText.draw(ctx, this.points);
+
+            this.livesStrText.draw(ctx, this.livesStr);
+            this.livesText.draw(ctx, this.lives);
         }
-
-        this.ball.draw(ctx);
-
-        this.pointsStrText.draw(ctx, this.pointsStr);
-        this.pointsText.draw(ctx, this.points);
-
-        this.livesStrText.draw(ctx, this.livesStr);
-        this.livesText.draw(ctx, this.lives);
+        else if (this.lives >= 0 && this.points < this.blocks.length){
+            this.youText.draw(ctx, this.youStr);
+            this.loseText.draw(ctx, this.loseStr);
+            this.restartText.draw(ctx, this.restartStr);
+        }
+        else {
+            this.youText.draw(ctx, this.youStr);
+            this.winText.draw(ctx, this.winStr);
+            this.restartText.draw(ctx, this.restartStr);
+        }
     }
 
     update(deltaTime) {
@@ -191,24 +223,52 @@ class Game {
         //Ball bounce on player
         if (boxOverlap(this.ball, this.player)){
             this.ball.velocity.y *= -1;
+            this.audio.play();
         }
 
-        //Block destroy
+        //Block destroy and game win logic
         for (let block of this.blocks){
             if (!block.isBroken && boxOverlap(this.ball, block)){
                 block.isBroken = true;
                 this.ball.velocity.y *= -1;
                 this.ball.position.y += this.ball.velocity.y * 10; //Move ball to avoid multiple block destruction
                 this.points ++;
+                if (this.points % blockCols == 0) {
+                    if (this.playerSize > 50) {
+                        this.playerSize -= 30;
+                        this.player = new Player(new Vector(this.player.position.x, canvasHeight - 50), this.playerSize, 20, "yellow");
+                    }
+                    if (playerSpeed < 0.8) playerSpeed += 0.07;
+                    if (ballSpeed < 0.7) ballSpeed += 0.07;
+                }
+                if (this.points >= this.blocks.length){
+                    this.gameOver = true;
+                }
+
+                this.audio.play();
             }
         }
 
-        //Life loss
+        //Life loss and Game over logic
         if (this.ball.position.y - this.ball.halfSize.y > canvasHeight) {
             this.ball.reset();
             this.lives --;
+            if (this.lives <= 0){
+                this.gameOver = true;
+            }
         }
 
+        //Game restart logic
+        if (this.gameRestart){
+            this.lives = 3;
+            this.points = 0;
+            this.playerSize = 200;
+            playerSpeed = 0.5;
+            ballSpeed = 0.4;
+            this.gameOver = false;
+            this.gameRestart = false;
+            this.initObjects(this.playerSize);
+        }
 
 
     }
